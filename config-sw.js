eee759c53e1f98280ed39c858094936d4c918bd4
Felix T. Vogel <contact@housebird.games>
2025-10-03 22:09:41 +0200

    Updated data; Updated Birdhouse

--- config-sw.js
+++ config-sw.js
@@ -1,5 +1,5 @@
 self.config = {
-  "version": "1.0.0.344-s",
+  "version": "1.0.0.349-s",
   "pageTitle": "Creations Manager",
   "cookieIdentifier": "creations_manager",
   "foundationYear": 2024,
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  "version": "1.0.0.344-s",
+  "version": "1.0.0.349-s",
   "pageTitle": "Creations Manager",
   "cookieIdentifier": "creations_manager",
   "foundationYear": 2024,
