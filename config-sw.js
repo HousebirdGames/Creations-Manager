c9e55f3e25e4aaf69818d0d8a17a591ee5dcbc4e
Felix T. Vogel <contact@housebird.games>
2025-12-12 15:01:18 +0100

    Updated data

--- config-sw.js
+++ config-sw.js
@@ -1,5 +1,5 @@
 self.config = {
-  "version": "1.0.0.361-s",
+  "version": "1.0.0.375-s",
   "pageTitle": "Creations Manager",
   "cookieIdentifier": "creations_manager",
   "foundationYear": 2024,
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  "version": "1.0.0.361-s",
+  "version": "1.0.0.375-s",
   "pageTitle": "Creations Manager",
   "cookieIdentifier": "creations_manager",
   "foundationYear": 2024,
