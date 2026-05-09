56e6e53c7723160a8ee88025ae21a29a16084e19
Felix T. Vogel <contact@housebird.games>
2026-05-09 23:04:49 +0200

    Updated data

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  "version": "1.0.0.384-s",
+  "version": "1.0.0.385-s",
   "pageTitle": "Creations Manager",
   "cookieIdentifier": "creations_manager",
   "foundationYear": 2024,
